0c27e141f52bc0cfddc1fb0a1778a25f66f1a061
Alexander Rudkovskiy <xandr.rudkovsky@gmail.com>
2021-07-06 22:22:39 +0300

    add feature for change a format status-bar string

--- print_status.py
+++ print_status.py
@@ -2,6 +2,10 @@ import time
 
 
 class PrintStatus:
+    status_bar_fmt = '\r|<bar><space>| - <current> из <max>'
+    symbol_fill = '#'
+    symbol_space = ' '
+
     def __init__(self, statuses: list):
         self.statuses = statuses
         self.generator = self._generator()
@@ -15,9 +19,17 @@ class PrintStatus:
     def print_status_bar(current_iter=0, max_iter=0, size=20, **kwargs):
         fill_value = int(size * current_iter / max_iter)
         space_value = int(size * (1 - current_iter / max_iter))
-        print(f'\r|{ "#" * fill_value }{ " " * space_value }| - { current_iter }', end='')
+        print(PrintStatus._get_status_bar_string(fill_value, space_value, current_iter, max_iter), end='')
         if current_iter == max_iter:
             print()
+
+    @staticmethod
+    def _get_status_bar_string(fill_value, space_value, current_iter, max_iter):
+        status_bar = PrintStatus.status_bar_fmt.replace('<bar>', PrintStatus.symbol_fill * fill_value)
+        status_bar = status_bar.replace('<space>', PrintStatus.symbol_space * space_value)
+        status_bar = status_bar.replace('<current>', str(current_iter))
+        status_bar = status_bar.replace('<max>', str(max_iter))
+        return status_bar
 
     def next(self, status_bar=False, **kwargs):
         if status_bar:
@@ -70,5 +82,3 @@ print_status.next()
 time.sleep(1)
 print_status.next()
 time.sleep(1)
-
-
